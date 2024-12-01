08b40797f925c180f42af674bf26f1245a707e27
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2024-12-01 18:16:32 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -22,5 +22,19 @@ int main() {
 
 	}
 	double ireal = 0;//ireal for f(x)=sin(4*pi*x) in [0,2] is zero
-	std::cout << std::setprecision(8) << std::abs(ireal - sum) << '\n';
+	std::cout << "Itrap: " << std::setprecision(8) << sum << '\n';
+	std::cout <<"ERROR: " << std::setprecision(8) << std::abs(ireal - sum) << '\n';
+	std::cout << "\n\n\n";
+	h = 0.1;
+	xi = 0.0;
+	//iterations = static_cast<size_t>((b - a) / h); (Again)
+	sum = 0;
+	for (size_t i = 0; i < iterations - 1; i++) {
+		double isimp = (f(xi) + 4 * f((xi + xi + h) / 2) + f(xi + h))*(h/6);
+		sum += isimp;
+		xi += h;
+	}
+
+	std::cout << "ISIMP: " << std::setprecision(8) << sum << '\n';
+	std::cout << "ERROR: " << std::setprecision(8) << std::abs(ireal - sum) << '\n';
 }
